76acdc803590879cbf8b4a7f9de9bfae0a1d6d0a
roman.kudin <roman.kudin@yahoo.com>
2020-04-03 16:23:33 +0300

    Filename fix2

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 include 'core/core.php';
 
 $input = 'res/data.xml';     //вхідний XML файл
-$output = 'res/data.csv';    //вихідний CSV файл
+$output_path = 'res/';    //вихідний CSV файл
 
 $parse = new XmlToCsv;
 $fields = $parse->parsing($input);
-$csv = $parse->generate_csv($fields, $output);
+$csv = $parse->generate_csv($fields, $output_path);
 
 if ($csv == True) {
     echo 'CSV file generated and created.';
